7072067c7d7fcce253f5d064ffd603b98c939df9
magnet88jp <magnet88jp@gmail.com>
2012-06-14 22:42:04 +0900

    add excel download

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,10 +7,21 @@ import java.util.*;
 
 import models.*;
 
+@With(ExcelControllerHelper.class)
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<DivaCalendar> calendars = 
+            DivaCalendar.find("order by category").fetch();
+        render(calendars);
+    }
+
+    public static void excel() {
+        List<DivaCalendar> calendars = 
+            DivaCalendar.find("order by category").fetch();
+        request.format = "xls";
+        String __FILE_NAME__ = "calendar.xls";
+        render(__FILE_NAME__, calendars);
     }
 
 }
